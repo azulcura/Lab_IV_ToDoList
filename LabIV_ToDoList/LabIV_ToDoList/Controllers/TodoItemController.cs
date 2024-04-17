68f0508afa887f361ee2c3974337d51242de6214
AzulCura <azulcura1@gmail.com>
2024-04-17 19:49:47 -0300

    Add Controllers

--- LabIV_ToDoList/LabIV_ToDoList/Controllers/TodoItemController.cs
+++ LabIV_ToDoList/LabIV_ToDoList/Controllers/TodoItemController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LabIV_ToDoList.Models;
+using LabIV_ToDoList.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LabIV_ToDoList.Controllers
@@ -7,5 +9,62 @@ namespace LabIV_ToDoList.Controllers
     [ApiController]
     public class TodoItemController : ControllerBase
     {
+        private readonly ITodoItemServices _todoItemServices;
+
+        public TodoItemController(ITodoItemServices todoItemServices)
+        {
+            _todoItemServices = todoItemServices;
+        }
+
+        [HttpGet]
+        public IActionResult GetTodoItems()
+        {
+            var todoItems = _todoItemServices.GetAllTodoItems();
+            return Ok(todoItems);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTodoItem(int id)
+        {
+            var todoItem = _todoItemServices.GetTodoItemById(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(todoItem);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateTodoItem(int id, [FromBody] TodoItemDto itemDto)
+        {
+            var updatedItem = _todoItemServices.UpdateTodoItem(id, itemDto);
+            if (updatedItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(updatedItem);
+        }
+        [HttpPost]
+        public IActionResult AddTodoItem(TodoItemDto item)
+        {
+            var newItemId = _todoItemServices.AddTodoItem(item);
+
+            var todoItemResponse = new
+            {
+                id = newItemId
+            };
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = newItemId }, todoItemResponse);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTodoItem(int id)
+        {
+            var result = _todoItemServices.DeleteTodoItem(id);
+            if (result == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
     }
 }
--- LabIV_ToDoList/LabIV_ToDoList/Controllers/UserController.cs
+++ LabIV_ToDoList/LabIV_ToDoList/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LabIV_ToDoList.Models;
+using LabIV_ToDoList.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LabIV_ToDoList.Controllers
@@ -7,5 +9,73 @@ namespace LabIV_ToDoList.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserServices _userServices;
+
+        public UserController(IUserServices userServices)
+        {
+            _userServices = userServices;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _userServices.GetAllUsers();
+
+            foreach (var user in users)
+            {
+                user.TodoItems = _userServices.GetTodoItemsByUserId(user.id);
+            }
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = _userServices.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(UserDto user)
+        {
+            var newUser = _userServices.AddUser(user);
+
+            var userResponse = new
+            {
+                id = newUser.id,
+                name = newUser.name,
+                email = newUser.email,
+                address = newUser.address
+            };
+
+            return CreatedAtAction(nameof(GetUser), new { id = newUser.id }, userResponse);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, [FromBody] UserDto userDto)
+        {
+            var updatedUser = _userServices.UpdateUser(id, userDto);
+            if (updatedUser == null)
+            {
+                return NotFound();
+            }
+            return Ok(updatedUser);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var result = _userServices.DeleteUser(id);
+            if (result == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
     }
 }
